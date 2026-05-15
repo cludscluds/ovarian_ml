dd8eb21d640cc4f569eb5e95120fcdee32ff2a4f
cludscluds <159909830+cludscluds@users.noreply.github.com>
2026-05-15 12:09:13 +0300

    ч\

--- check.py
+++ check.py
@@ -1,12 +1,10 @@
-import torch      # Подключаем нейросети
-import cv2        # Подключаем работу с видео/фото
-import numpy as np # Подключаем математику
+import torch      
+import cv2      
+import numpy as np 
 
-# Проверяем, видит ли Python установленные модули
-print("--- Проверка систем ---")
-print(f"PyTorch готов к работе? {torch.__version__ is not None}")
-print(f"OpenCV видит картинки? {cv2.__version__ is not None}")
-print(f"Математика (Numpy) работает? {np.__version__ is not None}")
 
-# Проверяем, есть ли у тебя видеокарта для ускорения (если нет - будет False, это норм)
-print(f"Доступно ли ускорение через видеокарту (CUDA): {torch.cuda.is_available()}")
+print(f"PyTorch? {torch.__version__ is not None}")
+print(f"OpenCV? {cv2.__version__ is not None}")
+print(f"Numpy? {np.__version__ is not None}")
+
+print(f"ускорение через видеокарту (CUDA): {torch.cuda.is_available()}")
